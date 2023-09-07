53be231ddf017a9d90bc24d622f88101593bb446
Adesh <adeshvaidya97@gmail.com>
2023-09-07 16:50:35 +0530

    Timer with beep sync functionality is completed

--- src/Features/Interview/Component/InterviewComponent.js
+++ src/Features/Interview/Component/InterviewComponent.js
@@ -9,12 +9,17 @@ const InterviewComponent = () => {
     sec: null
   });
 
+  const [secondsData, setSecondsData] = useState(0);
+
+  const [timeup, setTimeup] = useState("");
+
   const time = new Date();
 
   // console.log('time - ');
   // console.log(time);
 
   useEffect(() => {
+    setTimeup("");
 
     const intervalId = setInterval(() => {
       const time = new Date()
@@ -26,13 +31,41 @@ const InterviewComponent = () => {
     }, 1000);
 
     return () => clearInterval(intervalId);
+  }, [])
+
+  useEffect(() => {
     
   }, [])
+
+  //const sound = new Audio("D:\My Projects\interview_master\public\beep-07a.wav");
+
+  function nextQuestion() {
+    
+    setTimeup("");
+    var seconds = 10;
+    const countDown = setInterval(stopWatch, 1000);
+    
+    function stopWatch(){
+      if(seconds!==0){
+        seconds--;
+        setSecondsData(seconds);
+      }
+      if(seconds<3) {
+        const sound = new Audio("beep-07a.wav");
+        sound.play().catch((err) => {console.log('Error - ', err);});
+      }
+      if(seconds===0) {
+        clearInterval(countDown);
+        setTimeup("TIME UP");
+      }
+    }
+    
+  };
 
   return (
     <div className="container">
       <p className='display-4'>Coming Soon...</p>
-      <table className='timer'>
+      <small><table className='timer'>
         <tr className='borders'>
           <th className='border p-2'>Hr</th>
           <th className='border p-2'>Min</th>
@@ -43,7 +76,13 @@ const InterviewComponent = () => {
           <td className='min_ border p-2' style={{color: "green"}}>{currentTime.min}</td>
           <td className='sec_ border p-2' style={{color: "red"}}>{currentTime.sec}</td>
         </tr>
-      </table>
+        
+
+      </table></small>
+      <div className='sec_ countDown-circle move-top-right text-center' style={{color: "red", fontWeight: 'bold'}}>{secondsData}</div>
+        <p className='borders'>{timeup}</p>
+        <button type='button' className='btn btn-outline-secondary' onClick={nextQuestion}>Next</button>
+
 
       {/* <div className='circle my-5'></div> */}
     </div>
