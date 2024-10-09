4d17e28cf3421d131b722c4d0cda4bf960ac0d72
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-10-09 10:52:02 +0530

    Update CategoryApiHandler.js

--- src/Features/Categories/Service/CategoryApiHandler.js
+++ src/Features/Categories/Service/CategoryApiHandler.js
@@ -1,5 +1,7 @@
 import { BASE_URL, performApiRequest } from "../../../Service/CreateApi"
 
+// Category API Handler
+
 export const addCategoryApi = async (category) => {
     const resp = await performApiRequest.post(`${BASE_URL}/api/v1/category/new-category`, category);
     return resp.data;
